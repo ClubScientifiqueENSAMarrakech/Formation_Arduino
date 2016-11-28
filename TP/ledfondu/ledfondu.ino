22c96eda894306ed614eab94678d0d68d372fbb4
Imad Hsissou <imad.hsissou@gmail.com>
2016-11-28 13:48:29 +0000

    Update ledfondu.ino

--- TP/ledfondu/ledfondu.ino
+++ TP/ledfondu/ledfondu.ino
@@ -1,1 +1,26 @@
+// Variable pour la valeur de PWM.
+int value = 0;
+// La broche branchee a la DEL.
+int ledpin = 9;
 
+void setup()
+{
+        pinMode(9,OUTPUT);
+}
+
+void loop()
+{
+        //Fade in.
+        for(value = 0 ; value <= 255; value+=5)
+        {
+                analogWrite(ledpin, value);
+                delay(30);
+        }
+
+        // Fade out.
+        for(value = 255; value >=0; value-=5)
+        {
+                analogWrite(ledpin, value);
+                delay(30);
+        }
+}
