05bbeb34793b720e2cd2e2eddffeba9e574c8a59
Imad Hsissou <imad.hsissou@gmail.com>
2016-12-11 13:55:27 +0000

    Create FeuRouge.ino

---
+++ TP/FeuRouge/FeuRouge.ino
@@ -0,0 +1,26 @@
+/* TP Feu de signalisation Routière, durant le Formation Arduino
+   Club Scientifique ENSA Marrakech, SciENSA */
+
+unsigned int red_1 = 12;
+unsigned int red_2 = 7;
+
+unsigned int green_1 = 10;
+unsigned int green_2 = 8;
+
+unsigned int orange_1 = 11;
+unsigned int orange_2 = 9;
+
+/* votre code ici */
+
+
+void setup() {
+
+  /* votre code ici */
+}
+
+
+void loop() {
+
+  /* votre code ici */
+  
+}
