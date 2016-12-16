784c18fab29cd4ef97bc0fa7a951034e1776e982
Imad Hsissou <imad.hsissou@gmail.com>
2016-12-16 16:02:14 +0000

    Update FeuRouge.ino

--- Pratique/FeuRouge/FeuRouge.ino
+++ Pratique/FeuRouge/FeuRouge.ino
@@ -1,5 +1,7 @@
-/* TP Feu de signalisation Routière, durant le Formation Arduino
-   Club Scientifique ENSA Marrakech, SciENSA */
+/* 
+	TP Feu de signalisation Routière, durant le Formation Arduino
+	Club Scientifique ENSA Marrakech, SciENSA
+*/
 
 unsigned int red_1 = 12;
 unsigned int red_2 = 7;
@@ -10,17 +12,50 @@ unsigned int green_2 = 8;
 unsigned int orange_1 = 11;
 unsigned int orange_2 = 9;
 
-/* votre code ici */
-
+int state = LOW;
+int i = 0;
 
 void setup() {
-
-  /* votre code ici */
+                  
+  pinMode(red_1, OUTPUT);
+  pinMode(red_2, OUTPUT);
+  pinMode(green_1, OUTPUT);
+  pinMode(green_2, OUTPUT);
+  pinMode(orange_1, OUTPUT);
+  pinMode(orange_2, OUTPUT);
 }
 
-
 void loop() {
-
-  /* votre code ici */
   
+  digitalWrite(red_1, HIGH);
+  digitalWrite(red_2, LOW);
+
+  digitalWrite(green_1, LOW);
+  digitalWrite(green_2, HIGH);
+
+  digitalWrite(orange_1, LOW);
+  digitalWrite(orange_2, LOW);
+
+  delay(10000);
+  digitalWrite(green_2, LOW);
+  for (i = 0; i < 10; ++i)
+  { 
+    state = !state ;
+    digitalWrite(orange_2, state);
+            delay(500);
+  }
+  digitalWrite(red_1, LOW);
+  digitalWrite(green_1 , HIGH);
+
+  digitalWrite(red_2 , HIGH);
+  digitalWrite(orange_2, LOW);
+
+  delay(10000);
+  digitalWrite(green_1 , LOW);
+  for (i = 0; i < 10; ++i)
+  { 
+    state = !state ;
+    digitalWrite(orange_1, state);
+            delay(500);
+  }
 }
